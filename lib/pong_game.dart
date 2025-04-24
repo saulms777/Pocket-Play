5f0b56cdccc8b23a289b2d444eb937812b14c2b6
saulms <zhsamuel11@gmail.com>
2025-04-24 12:28:21 -0400

    added dashed middle line

--- lib/pong_game.dart
+++ lib/pong_game.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
@@ -16,8 +18,8 @@ class _PongGameState extends State<PongGame>
   static const double _puckSize = 20;
   static const double _puckSpeed = 3;
 
-  double _screenWidth = -1;
-  double _screenHeight = -1;
+  static double _screenWidth = -1;
+  static double _screenHeight = -1;
 
   late _Rectangle _player1;
   late _Rectangle _player2;
@@ -27,38 +29,61 @@ class _PongGameState extends State<PongGame>
   double _puckDx = 1;
   double _puckDy = 1;
 
+  void _initialize(BuildContext context) {
+    _screenWidth = MediaQuery.of(context).size.width;
+    _screenHeight = MediaQuery.of(context).size.height - kToolbarHeight;
+    _player1 = _Rectangle(
+        (_screenWidth - _platformWidth) / 2,
+        _platformSpacing,
+        _platformWidth, _platformHeight
+    );
+    _player2 = _Rectangle(
+        (_screenWidth - _platformWidth) / 2,
+        _screenHeight.toInt() - _platformSpacing - _platformHeight,
+        _platformWidth, _platformHeight
+    );
+    _puck = _Rectangle(
+        ((_screenWidth - _puckSize) / 2).roundToDouble(),
+        ((_screenHeight - _puckSize) / 2).roundToDouble(),
+        _puckSize, _puckSize
+    );
+  }
+
   void _onClick(double x, double y) {
+    x = max(x, _platformWidth / 2);
+    x = min(x, _screenWidth - _platformWidth / 2);
     x -= _platformWidth / 2;
+
     if (y < _screenHeight / 2) {
-      _player1.x += (x - _player1.x) / 2;
+      _player1.left += (x - _player1.left) / 2;
     }
     else {
-      _player2.x += (x - _player2.x) / 2;
+      _player2.left += (x - _player2.left) / 2;
     }
     setState(() {});
   }
 
   void _movePuck() {
     for (int i = 0; i < _puckSpeed; i++) {
-      _puck.x += _puckDx;
-      _puck.y += _puckDy;
+      _puck.left += _puckDx;
+      _puck.top += _puckDy;
 
-      bool outOfBoundsX = _puck.left() <= 0 || _puck.right() >= _screenWidth;
-      bool outOfBoundsY = _puck.top() <= 0 || _puck.bottom() >= _screenHeight;
+      bool outOfBoundsX = _puck.left <= 0 || _puck.right >= _screenWidth;
+      bool outOfBoundsY = _puck.top <= 0 || _puck.bottom >= _screenHeight;
       bool player1FrontCollide =
-          _puck.top() == _player1.bottom() &&
-          _puck.right() > _player1.left() && _puck.left() < _player1.right();
+          _puck.top == _player1.bottom &&
+          _puck.right > _player1.left && _puck.left < _player1.right;
       bool player1SideCollide =
-          (_puck.right() == _player1.left().roundToDouble() ||
-              _puck.left() == _player1.right().roundToDouble()) &&
-          _puck.top() < _player1.bottom() && _puck.bottom() > _player1.top();
+          (_puck.right == _player1.left.roundToDouble() ||
+              _puck.left == _player1.right.roundToDouble()) &&
+          _puck.top < _player1.bottom && _puck.bottom > _player1.top;
       bool player2FrontCollide =
-          _puck.bottom() == _player2.top() &&
-          _puck.right() > _player2.left() && _puck.left() < _player2.right();
+          _puck.bottom == _player2.top &&
+          _puck.right > _player2.left && _puck.left < _player2.right;
       bool player2SideCollide =
-          (_puck.right() == _player2.left().roundToDouble() ||
-              _puck.left() == _player2.right().roundToDouble()) &&
-          _puck.top() < _player2.bottom() && _puck.bottom() > _player2.top();
+          (_puck.right == _player2.left.roundToDouble() ||
+              _puck.left == _player2.right.roundToDouble()) &&
+          _puck.top < _player2.bottom && _puck.bottom > _player2.top;
 
       if (outOfBoundsX || player1SideCollide || player2SideCollide) {
         _puckDx *= -1;
@@ -86,26 +111,7 @@ class _PongGameState extends State<PongGame>
 
   @override
   Widget build(BuildContext context) {
-    if (_screenWidth == -1) {
-      _screenWidth = MediaQuery.of(context).size.width;
-      _screenHeight = MediaQuery.of(context).size.height - kToolbarHeight;
-      _player1 = _Rectangle(
-          (_screenWidth - _platformWidth) / 2,
-          _platformSpacing,
-          _platformWidth, _platformHeight
-      );
-      _player2 = _Rectangle(
-          (_screenWidth - _platformWidth) / 2,
-          _screenHeight.toInt() - _platformSpacing - _platformHeight,
-          _platformWidth, _platformHeight
-      );
-      _puck = _Rectangle(
-          ((_screenWidth - _puckSize) / 2).roundToDouble(),
-          ((_screenHeight - _puckSize) / 2).roundToDouble(),
-          _puckSize, _puckSize
-      );
-    }
-
+    if (_screenWidth == -1) _initialize(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -121,9 +127,15 @@ class _PongGameState extends State<PongGame>
               width: double.infinity,
               height: double.infinity,
             ),
+            CustomPaint(
+              size: Size(
+                _screenWidth, 10
+              ),
+              painter: _DottedLinePainter(),
+            ),
             Positioned(
-              left: _puck.x,
-              top: _puck.y,
+              left: _puck.left,
+              top: _puck.top,
               child: Container(
                 width: _puck.width,
                 height: _puck.height,
@@ -134,8 +146,8 @@ class _PongGameState extends State<PongGame>
               ),
             ),
             Positioned(
-              left: _player1.x,
-              top: _player1.y,
+              left: _player1.left,
+              top: _player1.top,
               child: Container(
                 width: _player1.width,
                 height: _player1.height,
@@ -143,8 +155,8 @@ class _PongGameState extends State<PongGame>
               ),
             ),
             Positioned(
-              left: _player2.x,
-              top: _player2.y,
+              left: _player2.left,
+              top: _player2.top,
               child: Container(
                 width: _player2.width,
                 height: _player2.height,
@@ -159,14 +171,38 @@ class _PongGameState extends State<PongGame>
 }
 
 class _Rectangle {
-  double x;
-  double y;
+  double left;
+  double top;
   final double width;
   final double height;
-  _Rectangle(this.x, this.y, this.width, this.height);
+  _Rectangle(this.left, this.top, this.width, this.height);
 
-  double left() => x;
-  double right() => x + width;
-  double top() => y;
-  double bottom() => y + height;
+  double get right => left + width;
+  double get bottom => top + height;
+}
+
+class _DottedLinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 10;
+
+    double dashWidth = 20;
+    double dashSpace = 10;
+    double startX = 0;
+    double height = _PongGameState._screenHeight / 2 - 5;
+
+    while (startX < size.width) {
+      canvas.drawLine(
+        Offset(startX, height + size.height / 2),
+        Offset(startX + dashWidth, height + size.height / 2),
+        paint,
+      );
+      startX += dashWidth + dashSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
